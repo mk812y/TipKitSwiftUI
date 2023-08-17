e72ae72b0c0aa8ad19d010deea230c295a9a7239
mk812y <mk812y@gmail.com>
2023-08-17 19:13:37 +0200

    Initial Commit

---
+++ TipKitSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TipKitSwiftUI
+//
+//  Created by Михаил Куприянов on 17.8.23..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TipKitSwiftUI/TipKitSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipKitSwiftUIApp.swift
+//  TipKitSwiftUI
+//
+//  Created by Михаил Куприянов on 17.8.23..
+//
+
+import SwiftUI
+
+@main
+struct TipKitSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
